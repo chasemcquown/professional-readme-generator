fcb11147a189a1d2a3337db77b62f052191f62a3
chasem <cpmcquown@gmail.com>
2021-07-09 14:04:14 -0700

    adds additional logic

---
+++ Develop/README.md
@@ -0,0 +1,34 @@
+# README Generator
+
+## Description
+
+This application will generate a README.md file upon completion of prompts.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+Clone this repo.
+
+## Usage
+
+Follow the command line prompts.
+
+## Credits
+
+Chase McQuown
+
+## License
+
+Licensed under the MIT
+
+## Contributing
+
+Contributors: Chase McQuown  
+ GitHub: https://github.com/chasemcquown
+Email: cpmcquown@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,91 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+
+const inquirer = require('inquirer');
+
+const util = require('util');
+
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'fullname',
+        type: 'input',
+        message: 'Please enter your full name'
+
+    },
+    {
+        name: 'username',
+        type: 'input',
+        message: 'Please enter your GitHub username:'
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Enter your email address:'
+
+    },
+    {
+        name: 'year',
+        type: 'list',
+        message: 'Please select the current year',
+        choices: ['2021', '2022', '2023', '2024', '2025']
+    },
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is the name of your project?'
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Please provide a brief description of your project'
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'How do you install this project?'
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'How do you use this project?'
+    },
+    {
+        name: 'credits',
+        type: 'input',
+        message: 'List the the collaborators of this project, if applicable:'
+    },
+    {
+        name: 'license',
+        type: 'list',
+        message: 'Select a license:',
+        choices: ['Apache', 'MIT', 'BSD3', 'GPL']
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName,data,function(err) {
+        if (err) {
+            return err;
+        } else {
+            console.log("Your README.md file has been generated!");
+        };
+    })
+
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile("README.md",generateMarkdown(data))
+    })
+}
 
 // Function call to initialize app
 init();
